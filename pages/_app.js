8a1e63cf4f3e62e0038c8cd61043d09401f88d77
Mohammad Ali <aliparkour83@gmail.com>
2023-12-25 05:21:55 +0330

    Toastify added to project and its config has been set

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import Layout from '@/components/layout'
+import Layout from '@/components/layout/layout'
 import '@/styles/globals.css'
+import { ToastConfig } from '@/utils/toastConfig'
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <ToastConfig />
     </Layout>
   )
 }
