0a41b9c69dd99a101d7889ecac5fd4ceb79f4b82
Mohammad Ali <aliparkour83@gmail.com>
2023-12-27 10:41:59 +0330

    deployment bugs

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,31 +6,29 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 const NextAuthConfig = {
     providers: [
-        process.env.VERCEL_ENV === "preview"
-            ?
-            CredentialsProvider({
-                name: "Credentials",
-                async authorize(credentials) {
-                    const { email, password } = credentials;
-                    if (!email || !password) {
-                        throw new Error("Please enter valid data");
-                    }
-
-                    try {
-                        await connectDB();
-                    } catch (err) {
-                        throw new Error("Error connecting to database")
-                    }
-                    const user = await User.findOne({ email: email });
-                    if (!user) throw new Error("User does not exist");
-
-                    const isValid = await verifyPassword(password, user.password);
-                    if (!isValid) throw new Error("Email or Password is incorrect");
-
-                    return { email }
+        CredentialsProvider({
+            name: "Credentials",
+            async authorize(credentials) {
+                const { email, password } = credentials;
+                if (!email || !password) {
+                    throw new Error("Please enter valid data");
                 }
-            })
-    ]
+
+                try {
+                    await connectDB();
+                } catch (err) {
+                    throw new Error("Error connecting to database")
+                }
+                const user = await User.findOne({ email: email });
+                if (!user) throw new Error("User does not exist");
+
+                const isValid = await verifyPassword(password, user.password);
+                if (!isValid) throw new Error("Email or Password is incorrect");
+
+                return { email }
+            }
+        })
+    ],
 }
 
 export default NextAuth(NextAuthConfig)
