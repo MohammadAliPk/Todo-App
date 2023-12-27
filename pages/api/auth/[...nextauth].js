a80ebc106de3a9aaca06d9b32922654576dbdf6c
Mohammad Ali <aliparkour83@gmail.com>
2023-12-27 10:25:05 +0330

    deployment bugs fixed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,28 +6,30 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 const NextAuthConfig = {
     providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            async authorize(credentials, req) {
-                const { email, password } = credentials;
-                if (!email || !password) {
-                    throw new Error("Please enter valid data");
+        process.env.VERCEL_ENV === "preview"
+            ?
+            CredentialsProvider({
+                name: "Credentials",
+                async authorize(credentials) {
+                    const { email, password } = credentials;
+                    if (!email || !password) {
+                        throw new Error("Please enter valid data");
+                    }
+
+                    try {
+                        await connectDB();
+                    } catch (err) {
+                        throw new Error("Error connecting to database")
+                    }
+                    const user = await User.findOne({ email: email });
+                    if (!user) throw new Error("User does not exist");
+
+                    const isValid = await verifyPassword(password, user.password);
+                    if (!isValid) throw new Error("Email or Password is incorrect");
+
+                    return { email }
                 }
-
-                try {
-                    await connectDB();
-                } catch (err) {
-                    throw new Error("Error connecting to database")
-                }
-                const user = await User.findOne({ email: email });
-                if (!user) throw new Error("User does not exist");
-
-                const isValid = await verifyPassword(password, user.password);
-                if (!isValid) throw new Error("Email or Password is incorrect");
-
-                return { email }
-            }
-        })
+            })
     ]
 }
 
