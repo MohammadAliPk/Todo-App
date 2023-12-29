1a7ae7482b406a3676400ba294027d2cc7e3878f
Mohammad Ali <aliparkour83@gmail.com>
2023-12-29 11:03:04 +0330

    testing sth

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import HomePage from "@/components/templates/HomePage";
 import { getSession } from "next-auth/react";
 
 
-export default function Home({ session }) {
-  console.log(session);
+export default function Home() {
   return (
     <HomePage />
   )
@@ -15,18 +14,18 @@ export async function getServerSideProps(context) {
   const session = await getSession(context);
 
 
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: "/login",
-  //       permanent: false
-  //     }
-  //   }
-  // }
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false
+      }
+    }
+  }
 
   return {
     props: {
-      session
+
     }
   }
 } 
