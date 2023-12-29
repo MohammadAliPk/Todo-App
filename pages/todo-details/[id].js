2b84f2138dfb3e1ad86afd714b6c9887df9d287f
Mohammad Ali <aliparkour83@gmail.com>
2023-12-29 17:17:04 +0330

    debugging

--- pages/todo-details/[id].js
+++ pages/todo-details/[id].js
@@ -2,6 +2,7 @@
 import DetailsPage from '@/components/templates/DetailsPage';
 import User from '@/models/User';
 import { getSession } from 'next-auth/react';
+import { toast } from 'react-toastify';
 
 const Index = ({ todo }) => {
 
@@ -18,6 +19,22 @@ export async function getServerSideProps(context) {
     const { params: { id } } = context;
 
     const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false
+            }
+        }
+    }
+
+    try {
+        await connectDB();
+    } catch (err) {
+        toast.error(err.message)
+    }
+
     const user = await User.findOne({ email: session.user.email });
     const todos = await user.todos;
     const todo = JSON.parse(JSON.stringify(todos.find(i => String(i._id) === id)));
