07b4eb702bb870fa21b8170bff9a0e7c58f5e8c4
Mohammad Ali <aliparkour83@gmail.com>
2024-01-01 09:11:33 +0330

    debugging

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,47 @@
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+
+const NextAuthConfig = {
+    session: {
+        strategy: "jwt"
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            async authorize(credentials) {
+                const { email, password } = credentials;
+                if (!email || !password) {
+                    throw new Error("Please enter valid data");
+                }
+
+                try {
+                    await connectDB();
+                } catch (err) {
+                    throw new Error("Error connecting to database")
+                }
+                const user = await User.findOne({ email: email });
+                if (!user) throw new Error("User does not exist");
+
+                const isValid = await verifyPassword(password, user.password);
+                if (!isValid) throw new Error("Email or Password is incorrect");
+
+                return { email }
+            }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    secret: process.env.JWT_SECRET,
+    adapter: MongoDBAdapter(clientPromise),
+}
+
+export default NextAuth(NextAuthConfig)
